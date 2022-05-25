515fee648e8f74afe359dadf50b0763553518dd5
log1xx <ptimmonswxman@gmail.com>
2022-05-24 21:58:40 -0400

    updated alert locations list

--- discord.js
+++ discord.js
@@ -9,11 +9,20 @@ function postWebhook(title, description, url, image, wfo, alertStatus, event, lo
     try {
         var expireFormat = new Date(expire);
         var content;
+        var locsString = '[';
+
+        for (i = 0; i < locs.length; i++) {
+            if (i < locs.length - 1) {
+                locsString = locsString + locs[i].county + ' ' + locs[i].state + ', ';
+            } else {
+                locsString = locsString + locs[i].county + ' ' + locs[i].state + ']';
+            }
+        }
 
         if (alertStatus == 'issues' || alertStatus == 'updates' || alertStatus == 'corrects' || alertStatus == 'continues') {
-            content = wfo + " " + alertStatus + " " + title + " for " + locs + " til " + expireFormat.toString().replace(/GMT.+\(/, '').replace(/\)/, '');
+            content = wfo + " " + alertStatus + " " + title + " for " + locsString + " til " + expireFormat.toString().replace(/GMT.+\(/, '').replace(/\)/, '');
         } else {
-            content = wfo + " " + alertStatus + " " + title + " for " + locs;
+            content = wfo + " " + alertStatus + " " + title + " for " + locsString;
         }
         
         const alertEmbed = new MessageEmbed()
